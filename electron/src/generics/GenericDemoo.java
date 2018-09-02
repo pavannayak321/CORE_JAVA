8db7e2baee979094e474f91e5ab67548e129fd8c
SAKKU JADAV <pavannayakiictgmail.com>
2018-09-02 19:51:42 +0530

    maven configuration

---
+++ electron/src/generics/GenericDemoo.java
@@ -0,0 +1,24 @@
+
+package generics;
+@FunctionalInterface
+interface A
+{
+	//void show();
+	void show1(int x);
+	default  void method()
+	{
+		
+	}
+	static void show12()
+	{
+		
+	}
+}
+
+public class GenericDemoo
+{
+	public static void main(String[] args)
+	{
+	
+	}
+}
